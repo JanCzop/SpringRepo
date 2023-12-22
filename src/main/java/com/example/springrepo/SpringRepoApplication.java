23f02ab0ad4bc79b14956b24f4f2ddabfb9dfcc6
JanCzop <janek.czop@gmail.com>
2023-12-22 22:30:12 +0100

    Practiced scheduling annotation with some examples

--- src/main/java/com/example/springrepo/SpringRepoApplication.java
+++ src/main/java/com/example/springrepo/SpringRepoApplication.java
@@ -3,9 +3,11 @@ package com.example.springrepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.springrepo.PorscheBaseAPI"})
+@EnableScheduling // To enable Scheduling package
+@ComponentScan(basePackages = {"com.example.springrepo.Scheduling"})
 public class SpringRepoApplication {
 
     public static void main(String[] args) {
